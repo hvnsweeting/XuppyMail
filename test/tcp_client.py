cec3ebbde3cbea4c873be448af8f548078d8c28f
Viet Hung Nguyen <famihug@gmail.com>
2011-10-06 22:39:52 +0700

    first commit

--- test/tcp_client.py
+++ test/tcp_client.py
@@ -1,19 +1,20 @@
-#example
 
+#Simple stream echo client
+#This program quit after recieved message from server
 import socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("localhost", 5000))
-while True:
-	data = client_socket.recv(512)
-	if data == 'q' or data == 'Q':
-		client_socket.close()
-		break;
-	else:
-		print "Received: ", data
-		data = raw_input ("SEND(Type q or Q to quit):")
-		if data <> 'Q' and data <> 'q':
-			client_socket.send(data)
-		else:
-			client_socket.send(data)
-			client_socket.close()
-			break;
+
+#1: create a socket obj
+#AF_INET indicates request an IP socket (IPv4). AF_INET6 for IPv6
+#second arg: transport protocol type
+ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP sockets
+
+#2.Connect to an address+port
+ssock.connect(('', 54321))
+
+#3. Send message to server
+ssock.send("Hello\n")
+
+#4. Print the message recieved form server
+print "Received from server: " + ssock.recv(100)
+
+ssock.close()
---
+++ test/tcp_server.py
@@ -0,0 +1,15 @@
+
+#Simple stream echo server
+import socket
+
+sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+sock.bind( ('', 54321) )
+sock.listen(5)
+
+while True:
+	newsock, (remhost, remport) = sock.accept()
+	print "Connected to " + remhost + ":" + str(remport),
+	message = newsock.recv(100)
+	print " recieved from client: " + message
+	newsock.send(message)
+	newsock.close()

e479a3cddd71edb0389c7e9457b475274365c644
Viet Hung Nguyen <famihug@gmail.com>
2011-10-07 22:36:31 +0700

    first commit

---
+++ test/select_test.py
@@ -0,0 +1,17 @@
+import socket
+import select
+
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock1.connect(('', 54321))
+sock2.connect(('', 54321))
+
+while True:
+	rlist, wlist, elist = select.select( [sock1, sock2], [], [], 5)
+
+	if [rlist, wlist, elist] == [[], [], []]:
+		print "Five second elapsed"
+	else:
+		for sock in rlist:
+			print sock.recv(100)

2ce7bf5b67d0caeedd29bc128d1521b1f22c53d1
Viet Hung Nguyen <famihug@gmail.com>
2011-10-29 23:54:47 +0700

    add smtp implement by me

---
+++ test/simple_smtp.py
@@ -0,0 +1,22 @@
+import socket
+
+SMTP_PORT = 25
+
+class SMTP:
+	helo_resp = None
+	def __init__(self):
+		#empty
+
+	#moi ham tuong ung voi 1 SMTP command
+	def sendmail(self):
+		#empty
+	def helo(self, name=''):
+		#empty
+	def mail(self):
+		#empty
+	def rcpt(self):
+		#empty
+	def mail(self,msg):
+		#empty
+	def quit(self):
+		#empty

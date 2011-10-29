8b7d766704cee57f00346f5d754921e4fd844079
Viet Hung Nguyen <famihug@gmail.com>
2011-10-30 00:09:17 +0700

    change args

--- test/simple_smtp.py
+++ test/simple_smtp.py
@@ -4,19 +4,19 @@ SMTP_PORT = 25
 
 class SMTP:
 	helo_resp = None
-	def __init__(self):
+	def __init__(self, host='', port=0, local_hostname=None):
 		#empty
 
 	#moi ham tuong ung voi 1 SMTP command
-	def sendmail(self):
+	def sendmail(self, from_addr, to_addrs, msg):
 		#empty
 	def helo(self, name=''):
 		#empty
-	def mail(self):
+	def mail(self, sender):
 		#empty
-	def rcpt(self):
+	def rcpt(self, recip):
 		#empty
-	def mail(self,msg):
+	def data(self,msg):
 		#empty
 	def quit(self):
 		#empty

cb224ff416bcdd721740455faf23d3254bec951f
Viet Hung Nguyen <famihug@gmail.com>
2011-10-30 00:46:07 +0700

    implement almost SMTPs methods

--- test/simple_smtp.py
+++ test/simple_smtp.py
@@ -8,16 +8,53 @@ class SMTP:
 	def __init__(self, host='', port=0, local_hostname=None):
 		#empty
 
+	
+	def getreply:
+		#TODO
+	
+	def putcmd(self, cmd, args = ""):
+		if args == "":
+			str = '%s%s' % (cmd, CRLF)
+		else:
+			str = '%s %s %s' % (cmd, args, CRLF)
+		self.send(str)
+		#end putcmd
+
+	def docmd(self, cmd, args = ""):
+		"""Send a command and return its response code"""
+		self.putcmd(cmd, args)
+		return self.getreply()
+	
 	#moi ham tuong ung voi 1 SMTP command
 	def sendmail(self, from_addr, to_addrs, msg):
 		#empty
+
 	def helo(self, name=''):
-		#empty
+		"""SMTP 'helo' command.
+		default hostname to send defaults to the FQDN of the localhost.		"""
+		self.putcmd("HELO", name or self.local_hostname)
+		(code, msg) = self.getreply()
+		self.helo_resp = msg
+		return (code, msg)
+
 	def mail(self, sender):
-		#empty
+		""" SMTP 'MAIL' command"""
+		self.putcmd("MAIL", "FROM:%s" % (quoteaddr(sender)))
+		return self.getreply()
+
 	def rcpt(self, recip):
-		#empty
+		""" SMTP 'RCPT' command """
+		self.putcmd("RCPT", "TO:%s" % (quoteaddr(recip)))
+		return self.getreply()
+
 	def data(self,msg):
+		self.putcmd("DATA")
+		(code, repl) = self.getreply()
+		#TODO
 		#empty
+
 	def quit(self):
-		#empty
+		"""Terminate the SMTP session."""
+		res = self.docmd("quit")
+		self.close() #TODO implement later
+		return res
